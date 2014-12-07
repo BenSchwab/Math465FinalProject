1f51a2467f013507f050c8393dbd7375e253a1d4
= <benjamin.schwab@duke.edu>
2014-12-07 13:08:48 -0500

    refactored laplacian builder form spectral clusterer

--- LaplacianBuilder.m
+++ LaplacianBuilder.m
@@ -1,6 +1,57 @@
-function [ output_args ] = LaplacianBuilder( input_args )
-%LAPLACIANBUILDER Summary of this function goes here
-%   Detailed explanation goes here
+function [ G ] = LaplacianBuilder( X, UserOpts)
+%LAPLACIANBUILDER This function builds the Laplacian graph for the input
+%DxN matrix.
+
+%Default options
+Opts = {};
+Opts.NumberNeighbors = 5;
+Opts.NumClusters = 2;
+
+%Populate User options
+if nargin>1
+    fn = fieldnames(UserOpts);
+    for j=1:length(fn)
+        name = fn{j};
+        value = getfield(UserOpts, name);
+
+        if     strcmpi(name,'NumberNeighbors')   Opts.NumberNeighbors = value;
+        elseif strcmpi(name,'NumClusters')       Opts.NumClusters = value;
+       
+        end
+    end
+end
+
+d = size(X,1);
+n = size(X,2);
+
+[count,idxs, dists,NNInfo] = nrsearch(X, X, Opts.NumberNeighbors, 0);
+
+W = zeros(n,n);
+D = zeros(n,n);
+for i = 1:n
+    neighbors = idxs{i};
+    neighbor_distances = dists{i};
+    for j = 1:Opts.NumberNeighbors
+        neighbor = neighbors(j);
+        W(i, neighbor) = 1/(1 + neighbor_distances(j));
+        W(neighbor, i) = 1/(1 + neighbor_distances(j));
+    end
+end
+
+for i = 1:n
+    D(i,i) = sum(W(i,:)); 
+end
+L = D - W;
+
+
+LNormalized = D^(-1/2)*L*D^(-1/2);
+
+G.W = W;
+G.D = D;
+G.L = L;
+G.LRW = D^(-1)*L;
+G.LNormalized = LNormalized;
+
 
 
 end
--- SpectralClusterer.m
+++ SpectralClusterer.m
@@ -1,64 +1,16 @@
-function [ G ] = SpectralClusterer(X, userOpts)
-%CONSTRUCTUNORMALIZEDLAPLACIAN Construct an unormalized Laplacian for the 
-%data
-
-%Default options
-Opts = {};
-Opts.NumberNeighbors = 5;
-Opts.NumClusters = 2;
-
-%Populate User options
-if nargin>1
-    fn = fieldnames(userOpts);
-    for j=1:length(fn)
-        name = fn{j};
-        value = getfield(userOpts, name);
-
-        if     strcmpi(name,'NumberNeighbors')   Opts.NumberNeighbors = value;
-        elseif strcmpi(name,'NumClusters')       Opts.NumClusters = value;
-       
-        end
-    end
-end
-
-d = size(X,1);
-n = size(X,2);
-
-[count,idxs, dists,NNInfo] = nrsearch(X, X, Opts.NumberNeighbors, 0);
-
-W = zeros(n,n);
-D = zeros(n,n);
-for i = 1:n
-    neighbors = idxs{i};
-    neighbor_distances = dists{i};
-    for j = 1:Opts.NumberNeighbors
-        neighbor = neighbors(j);
-        W(i, neighbor) = 1/(1 + neighbor_distances(j));
-        W(neighbor, i) = 1/(1 + neighbor_distances(j));
-    end
-end
-
-for i = 1:n
-    D(i,i) = sum(W(i,:)); 
-end
-L = D - W;
+function [ clusters ] = SpectralClusterer(X, Opts)
 
 
-LNormalized = D^(-1/2)*L*D^(-1/2);
-[EigenVec,EigenVal] = eig(L); 
+G = LaplacianBuilder(X,Opts);
 
+
+
+
+[EigenVec,EigenVal] = eig(G.L); 
 U = EigenVec(:,1:Opts.NumClusters);
-labels = kmeans(U, Opts.NumClusters);
 
-G.W = W;
-G.D = D;
-G.L = L;
-G.U = U;
-G.labels = labels;
-G.LNormalized = LNormalized;
-G.EigenVec = EigenVec;
-G.EigenVal = EigenVal;
 
+clusters = kmeans(U, Opts.NumClusters);
 
 
 
