d315c3ef57a27d34e44506d847dfba9d7ce986a8
Avinash Moondra <avimoondra@gmail.com>
2014-12-07 13:45:26 -0500

    add variable to control radius of sphere in blobInSphere

--- SyntheticData/blobInSphere.m
+++ SyntheticData/blobInSphere.m
@@ -5,8 +5,9 @@ blobLabels = zeros(1,nBlob);
 
 sphere = normrnd(0,1,d,nSphere);
 lengths = sqrt(sum(sphere.^2));
+sphereRadius = 3;
 for i=1:nSphere
-    sphere(:,i) = 3*sphere(:,i)/lengths(i);
+    sphere(:,i) = sphereRadius*sphere(:,i)/lengths(i);
 end
 sphere = sphere + normrnd(0,0.2,d,nSphere);
 sphereLabels = ones(1,nSphere);
