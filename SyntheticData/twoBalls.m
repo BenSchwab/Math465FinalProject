9feb83eafd0297c8181c000c32bc9e2ede1b8fbd
Avinash Moondra <avimoondra@gmail.com>
2014-12-07 14:49:41 -0500

    clean up synthetic data code

--- SyntheticData/twoBalls.m
+++ SyntheticData/twoBalls.m
@@ -1,11 +1,10 @@
 function [ X, Y ] = twoBlobs(n, k, d)
 
-ballTwoCenter= zeros(d,n);
-ballTwoCenter(1,:) = 2*ones(1,n);
-
 ballOne = transpose(GenerateBall(n,k,d));
 ballOneLabels = zeros(1,n);
 
+ballTwoCenter= zeros(d,n);
+ballTwoCenter(1,:) = 2*ones(1,n);
 ballTwo = transpose(GenerateBall(n,k,d)) + ballTwoCenter;
 ballTwoLabels = ones(1,n);
 
