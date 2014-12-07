79db2b4996e0fd3e2c2911709251eb049aa21d3e
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-12-07 17:44:10 -0500

    color points now by cluster - see testSynthetic

--- GradientDescent.m
+++
@@ -1,5 +0,0 @@
-function [out] = GradientDescent(X, start, step)
-    numClusters = start;
-    G = SpectralClusterer(X, struct('NumClusters', numClusters, 'NumberNeighbors', 10));
-    purity = ClusterPurity(G.labels, Y, nc)
-end
