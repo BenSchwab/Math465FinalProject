737dd08d28c12e5b0c82c141913155f94b705b9a
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-12-08 21:59:11 -0500

    added testDense and modified params of dense

--- SyntheticData/denseAndSparseBlobs.m
+++ SyntheticData/denseAndSparseBlobs.m
@@ -3,9 +3,12 @@ function [X, Y] = denseAndSparseBlobs(n, dim)
     sigmaOne = 0.2;
     ballOne = normrnd(0, sigmaOne, dim, 1000);
 
-    distance = 5; %distance between centers of the two clusters
+    distance = 1; %distance between centers of the two clusters with high 
+    % variance on large cluster 
+    % from epirical study - changing this does not affect acurracy i.e.
+    % making closer does not help
     
-    sigmaTwo = 3;
+    sigmaTwo = 1;
     
     twoCenter = zeros([dim, 1]);
     twoCenter(1) = distance;
