44ad16f45f6ce174dec647eac60da3cd16a4cbaa
Hunter Nisonoff <hmn5@cs.duke.edu>
2014-12-10 15:07:25 -0500

    hunters commits

--- rewriteFiles.py
+++ rewriteFiles.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 import os
 path = os.getcwd()+'/mnist_figs/'
+NewPath = path+'MergeUnMergeFiles/'
 for filename in os.listdir(path):
-    original = open(path+filename)
-    outfileMerge = open(path+filename+'_merged','w')
-    outfileUnmerge = open(path+filename+'_unmerged','w')
-    Unmerged = True
-    for line in original:
-        if line[:6]=='merged':
-            Unmerged = False
-        if Unmerged == True:
-            outfileUnmerge.write(line)
-        else:
-            outfileMerge.write(line)
-    outfileMerge.close()
-    outfileUnmerge.close()
-    original.close()
+    if filename != 'MergeUnMergeFiles':
+        original = open(path+filename)
+        outfileMerge = open(NewPath+filename+'_merged','w')
+        outfileUnmerge = open(NewPath+filename+'_unmerged','w')
+        Unmerged = True
+        for line in original:
+            if line[:6]=='merged':
+                Unmerged = False
+            if Unmerged == True:
+                outfileUnmerge.write(line)
+            else:
+                outfileMerge.write(line)
+        outfileMerge.close()
+        outfileUnmerge.close()
+        original.close()
