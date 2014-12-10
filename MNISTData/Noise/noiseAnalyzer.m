adf30ce3dd3691741ec45dbc7ef68a70468bafa6
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-12-09 21:09:42 -0500

    added third column to Cluster Purity and mod noiseAnalyzer

--- MNISTData/Noise/noiseAnalyzer.m
+++ MNISTData/Noise/noiseAnalyzer.m
@@ -3,8 +3,8 @@
 %% Generally, what does noisy image data look like?
 
 queryDigits = [0,1,3,6];
-numberOfPoints = 400;
-noiseIter = 10;
+numberOfPoints = 200;
+noiseIter = 15;
 %noiseVector = (0:noiseIter)*10;
 imgOpts.imR = 28;
 imgOpts.imC = 28;
@@ -17,12 +17,12 @@ noiseVector = 0:20:300; %2.^(0:numTrials); %(0:20:2000);
 
 for i=1:(noiseIter+1)
     [X,Y] = MNIST_helper(queryDigits, numberOfPoints,noiseVector(i)); 
-    [clusters, ~] = SpectralClusterer(X, Y, struct('NumClusters',numberOfClusters,'NumberNeighbors',40,'auto',0));
+    [clusters, ~] = SpectralClusterer(X, Y, struct('NumClusters',numberOfClusters,'NumberNeighbors',30,'auto',0));
     purityMatrix = ClusterPurity(clusters.Unmerged, Y, numberOfClusters);
     purityMatrix
-    pause;
-    averagePurity = mean(purityMatrix(:,1));
+    averagePurity = sum(purityMatrix(:,1).*purityMatrix(:,3))/sum(purityMatrix(:,3));
     averagePurities(1,i) = averagePurity;
-    
-    pause;
 end
+averagePurities
+plot(noiseVector, averagePurities);
+title(sprintf('[0,1,3,6] clustery purity as a function of noise - nData=%d',numberOfPoints))
